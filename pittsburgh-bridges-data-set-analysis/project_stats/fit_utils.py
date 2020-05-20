c62641f2d48b75f2e13d2380c38bdc3156e18dac
franec94 <franec94@gmail.com>
2020-05-20 11:56:11 +0200

    created subdir project_stats for executing and monitoring project's status, and development across its evelution

---
+++ pittsburgh-bridges-data-set-analysis/project_stats/fit_utils.py
@@ -0,0 +1,241 @@
+print(__doc__)
+
+# =========================================================================== #
+# Imports
+# =========================================================================== #
+
+# Critical Imports
+# --------------------------------------------------------------------------- #
+import warnings; warnings.filterwarnings("ignore")
+
+# Imports through 'from' syntax
+# --------------------------------------------------------------------------- #
+from itertools import islice
+from pprint import pprint
+from sklearn import preprocessing
+
+# Standard Imports
+# --------------------------------------------------------------------------- #
+import copy; import os
+import sys; import shutil
+import time
+
+# Imports through 'as' syntax
+# --------------------------------------------------------------------------- #
+import numpy as np; import pandas as pd
+
+# Imports for graphics
+# --------------------------------------------------------------------------- #
+# Matplotlib pyplot provides plotting API
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from matplotlib.colors import ListedColormap
+import chart_studio.plotly.plotly as py
+import seaborn as sns; sns.set()
+
+
+# Imports sklearn
+# --------------------------------------------------------------------------- #
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+from sklearn.model_selection import train_test_split
+
+# Custom Imports
+# --------------------------------------------------------------------------- #
+from utils_functions import *
+
+# =========================================================================== #
+# Functions
+# =========================================================================== #
+
+# --------------------------------------------------------------------------- #
+# Training Functions
+# --------------------------------------------------------------------------- #
+
+def linear_regression_custom(X, y, test_size=0.33, random_state=42, randomize_data: bool = False, n_times: int = 6, scale: float = .1):
+    """
+    https://scikit-learn.org/stable/auto_examples/linear_model/plot_ols_ridge_variance.html#sphx-glr-auto-examples-linear-model-plot-ols-ridge-variance-py
+    """
+    if len(X.shape) == 1:
+        X = X[:, np.newaxis]
+    
+    # X_train, X_test, y_train, y_test = train_test_split(
+    X_train, X_test, y_train, _ = train_test_split( 
+        X, y, test_size=test_size, random_state=random_state)
+    
+    classifiers = dict(ols=linear_model.LinearRegression(),
+                   ridge=linear_model.Ridge(alpha=.1))
+
+    for name, clf in classifiers.items():
+        fig, ax = plt.subplots(figsize=(4, 3))
+        
+        if randomize_data:
+            for _ in range(n_times):
+                this_X = scale * np.random.normal(size=(X_train.shape[0], 1)) + X_train
+                clf.fit(this_X, y_train)
+
+                ax.plot(X_test, clf.predict(X_test), color='gray')
+                ax.scatter(this_X, y_train, s=3, c='gray', marker='o', zorder=10)
+                pass
+            pass
+        
+        clf.fit(X_train, y_train)
+        ax.plot(X_test, clf.predict(X_test), linewidth=2, color='blue')
+        ax.scatter(X_train, y_train, s=30, c='red', marker='+', zorder=10)
+
+        ax.set_title(name)
+        ax.set_xlim(0, 2)
+        ax.set_ylim((0, 1.6))
+        ax.set_xlabel('X')
+        ax.set_ylabel('y')
+
+        fig.tight_layout()
+    
+    plt.show()
+    pass
+
+
+def classifier_comparison(X, y, start_clf: int = 0, stop_clf: int = 10, verbose: int = 0, record_errors: bool = False) -> object:
+    
+    assert len(X.shape) == 2, "X must have at list two predictors"
+    
+    """
+    https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html#sphx-glr-auto-examples-classification-plot-classifier-comparison-py
+    """
+    h = .02  # step size in the mesh
+
+    names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
+         "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
+         "Naive Bayes", "QDA"]
+
+    classifiers = [
+        KNeighborsClassifier(3),
+        SVC(kernel="linear", C=0.025),
+        SVC(gamma=2, C=1),
+        GaussianProcessClassifier(1.0 * RBF(1.0)),
+        DecisionTreeClassifier(max_depth=5),
+        RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+        MLPClassifier(alpha=1, max_iter=1000),
+        AdaBoostClassifier(),
+        GaussianNB(),
+        QuadraticDiscriminantAnalysis()
+    ]
+    
+    assert start_clf >= 0
+    assert stop_clf > 0
+    assert start_clf < stop_clf
+    assert stop_clf <= len(classifiers)
+    
+    error_list: list = list()
+    
+    # rng = np.random.RandomState(2)
+    # X += 2 * rng.uniform(size=X.shape)
+    linearly_separable = (X, y)
+
+    datasets = [
+        # make_moons(noise=0.3, random_state=0),
+        # make_circles(noise=0.2, factor=0.5, random_state=1),
+        linearly_separable
+    ]
+
+    # _ = plt.figure(figsize=(27, 9)) # figure
+    # _ = plt.figure(figsize=(10, 10)) # figure
+    _ = plt.figure() # figure
+    i = 1
+    # iterate over datasets
+    for ds_cnt, ds in enumerate(datasets):
+        # preprocess dataset, split into training and test part
+        X, y = ds
+        X = StandardScaler().fit_transform(X)
+        X_train, X_test, y_train, y_test = \
+            train_test_split(X, y, test_size=.4, random_state=42)
+
+        x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+        y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+        # just plot the dataset first
+        cm = plt.cm.RdBu
+        cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+        ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+        if ds_cnt == 0:
+            ax.set_title("Input data")
+        # Plot the training points
+        ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
+                   edgecolors='k')
+        # Plot the testing points
+        ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6,
+                   edgecolors='k')
+        ax.set_xlim(xx.min(), xx.max())
+        ax.set_ylim(yy.min(), yy.max())
+        ax.set_xticks(())
+        ax.set_yticks(())
+        i += 1
+        
+        if type(names[start_clf:stop_clf]) is not list:
+            names_ = [names[start_clf:stop_clf]]
+        else:
+            names_= names[start_clf:stop_clf]
+        
+        if type(classifiers[start_clf:stop_clf]) is not list:
+            classifiers_ = [classifiers[start_clf:stop_clf]]
+        else:
+            classifiers_ = classifiers[start_clf:stop_clf]
+                            
+        for name, clf in zip(names_, classifiers_):
+            # for name, clf in zip(names, classifiers):
+            try:
+                verbose_message(message=f"Classifier: {name}", verbose=verbose, header_flag=True)
+                ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+                clf.fit(X_train, y_train)
+                score = clf.score(X_test, y_test)
+
+                # Plot the decision boundary. For that, we will assign a color to each
+                # point in the mesh [x_min, x_max]x[y_min, y_max].
+                if hasattr(clf, "decision_function"):
+                    Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+                else:
+                    Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+
+                # Put the result into a color plot
+                Z = Z.reshape(xx.shape)
+                ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+                # Plot the training points
+                ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
+                           edgecolors='k')
+                # Plot the testing points
+                ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                           edgecolors='k', alpha=0.6)
+
+                ax.set_xlim(xx.min(), xx.max())
+                ax.set_ylim(yy.min(), yy.max())
+                ax.set_xticks(())
+                ax.set_yticks(())
+                if ds_cnt == 0:
+                    ax.set_title(name)
+                ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                    size=15, horizontalalignment='right')
+                i += 1
+                pass
+            except Exception as err:
+                record_error((name, err), error_list=error_list, record_errors=record_errors)
+                pass
+        pass
+
+    plt.tight_layout()
+    plt.show()
+    return error_list
---
+++ pittsburgh-bridges-data-set-analysis/project_stats/project_stats_utils.py
@@ -0,0 +1,266 @@
+print(__doc__)
+
+# =========================================================================== #
+# Imports
+# =========================================================================== #
+
+# Critical Imports
+# --------------------------------------------------------------------------- #
+import warnings; warnings.filterwarnings("ignore")
+
+# Imports through 'from' syntax
+# --------------------------------------------------------------------------- #
+from itertools import islice
+from pprint import pprint
+from sklearn import preprocessing
+
+
+# Standard Imports
+# --------------------------------------------------------------------------- #
+import copy; import os
+import sys; import shutil
+import time
+
+
+# Imports through 'as' syntax
+# --------------------------------------------------------------------------- #
+import numpy as np; import pandas as pd
+
+# Matplotlib pyplot provides plotting API
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+import chart_studio.plotly.plotly as py
+import seaborn as sns; sns.set()
+
+# =========================================================================== #
+# Functions
+# =========================================================================== #
+
+# --------------------------------------------------------------------------- #
+# Fetch Input data Functions
+# --------------------------------------------------------------------------- #
+
+def dir_traversal_by_os_walk(root_dir_path: str, verbose: int = 0, filter_access_files: bool = False) -> list:
+    resources_list: list = [(root, dirs, files) for root, dirs, files, in os.walk(root_dir_path)]  
+    
+    if verbose == 1:
+        print("List of all sub-directories and files:")  
+        for (root, dirs, files)  in resources_list: 
+            print('Root:', root)
+            print('Directories:', dirs)
+            print('Files:', files)
+            pass
+        pass
+    
+    if filter_access_files is True:
+        def check_permission_file(a_file) -> bool:
+            return  os.access(a_file, os.F_OK) and \
+                (os.access(a_file, os.R_OK) \
+                or os.access(a_file, os.W_OK) \
+                or os.access(a_file, os.X_OK))
+        res = []
+        for _, (root, dirs, files) in enumerate(resources_list):
+            files_tmp = list(filter(check_permission_file, files))
+            res.append((root, dirs, files_tmp))
+            pass
+        resources_list = res
+        pass
+    return resources_list
+
+# --------------------------------------------------------------------------- #
+# Create Dataframes Functions
+# --------------------------------------------------------------------------- #
+
+def get_df_from_list_of_os_walk_numeric(resources_list: list, columns="root,dirs,files", verbose: int = 1) -> pd.DataFrame:
+    if type(columns) is not list:
+        columns = "root,dirs,files".split(",")
+    stats_list: list = list(map(lambda record: (record[0], len(record[1]), len(record[2])), resources_list))
+    df: pd.DataFrame = pd.DataFrame(data=stats_list, columns=columns)
+    return df
+
+
+
+def get_df_from_list_of_os_walk(resources_list: list, columns="root,dirs,files,files size", verbose: int = 1) -> pd.DataFrame:
+    data: list = list()
+    if type(columns) is not list:
+        columns = columns.split(",")
+    for _, (root, _, files) in enumerate(resources_list): # (root, dirs, files)
+        for _, a_file in enumerate(files):
+            a_record: list = [root, os.path.dirname(a_file), os.path.basename(a_file), os.path.getsize(os.path.join(root, a_file))]
+            data.append(a_record)
+            pass
+        pass
+    df: pd.DataFrame = pd.DataFrame(data=data, columns=columns)
+    return df
+
+
+def get_df_from_list_of_os_walk_numeric_indexed(resources_list: list, columns="dirs,files", verbose: int = 1) -> pd.DataFrame:
+    if type(columns) is not list:
+        columns = columns.split(",")
+    stats_list: list = list(map(lambda record: (len(record[1]), len(record[2])), resources_list))
+    index_list: list = list(map(lambda record: record[0], resources_list))
+    
+    df: pd.DataFrame = pd.DataFrame(data=stats_list, columns=columns, index=index_list)
+    return df
+
+
+def get_df_from_list_of_os_walk_numeric_indexed_v2(resources_list: list, columns="dirs,dirs_size,files", verbose: int = 1) -> pd.DataFrame:
+    if type(columns) is not list:
+        columns = columns.split(",")
+    
+    def get_size_all_files(root, files_list: list) -> int:
+        tot_size = 0
+        for _, a_file in enumerate(list(map(lambda xi: os.path.join(root, xi), files_list))):
+            try:
+                if os.path.exists(a_file) and os.path.isfile(a_file):
+                    tot_size = tot_size + os.path.getsize(a_file)
+            except: pass
+        return tot_size
+    
+    stats_list: list = list(map(lambda record: (len(record[1]), get_size_all_files(record[0], record[2]), len(record[2])), resources_list))
+    index_list: list = list(map(lambda record: record[0], resources_list))
+    
+    df: pd.DataFrame = pd.DataFrame(data=stats_list, columns=columns, index=index_list)
+    return df
+
+# --------------------------------------------------------------------------- #
+# Show Graphs Functions
+# --------------------------------------------------------------------------- #
+
+def show_histograms_by_scaler_tech(df: pd.DataFrame, variable_name: str, rescale_data_techs: list, save_fig=False, rescale_data_tech: str=None, dest_fig: str="stats_report_figures", fig_name: str="all_hists_chart.ong", figsize=None, show_default=False):
+    
+    assert df is not None, "df is None"
+    assert variable_name in df.columns, f"variable name: {variable_name} is not a column name of input dataframe"
+    
+    check_and_create_dirs(dest_fig)
+
+    n = len(rescale_data_techs)
+    nrows = n // 2 if n % 2 == 0 else n // 2 + 1
+    ncols = 2
+    
+    if figsize is not None: plt.figure(figsize=figsize)
+    else: plt.figure()
+    
+    if show_default is True:
+        index = 1
+        plt.subplot(nrows, ncols, 1)
+        meta_data_img = {
+                'title': f'Hist',
+                'ylabel': 'Freq',
+                'xlabel': 'Ext'
+        }
+        show_histogram_by_variable_from_df(
+            df, variable_name=variable_name,
+            fig_name="plain_hist.png",
+            meta_data_img=meta_data_img)
+    else: index = 0
+    for _, scaler_tech in enumerate(rescale_data_techs):
+        
+        # index = 1 if ii % 2 == 0 else 2
+        index = index + 1
+        plt.subplot(nrows, ncols, index)
+        meta_data_img = {
+                'title': f'Hist|{scaler_tech.lower()}',
+                'ylabel': 'Freq',
+                'xlabel': 'Ext'
+        }
+        show_histogram_by_variable_from_df(
+            df, variable_name=variable_name,
+            rescale_data_tech=scaler_tech,
+            fig_name="plain_hist_{}.png".format(scaler_tech.lower()),
+            meta_data_img=meta_data_img)
+        # if index == 2: nrows = nrows + 1
+        pass
+    plt.show()
+    if save_fig is True:
+        plt.savefig(os.path.join(dest_fig, fig_name))
+        pass
+    pass
+
+
+def show_histogram_by_variable_from_df(df: pd.DataFrame, variable_name: str, save_fig=False, rescale_data_tech: str=None, dest_fig: str="stats_report_figures", fig_name="a_hist_chart.png", meta_data_img: dict=None) -> None:
+    
+    assert df is not None, "df is None"
+    assert variable_name in df.columns, f"variable name: {variable_name} is not a column name of input dataframe"
+    
+    check_and_create_dirs(dest_fig)
+    
+    labels_list = "title,ylabel,xlabel".split(",")
+    values_list = "Histogram,Y,X".split(",")
+    
+    predictor = df[f"{variable_name}"].value_counts()
+    if rescale_data_tech is not None:
+        tech_names: list = "MinMaxScaler,StandardScaler,normalize".lower().split(",")
+        tech_list: list = [preprocessing.MinMaxScaler(),preprocessing.StandardScaler(), preprocessing.normalize]
+        tech_dict: dict = dict(zip(tech_names, tech_list))
+        
+        rescaled_values = check_perform_rescaling_technique(predictor.values, rescale_data_tech, tech_dict)
+     
+        ax = sns.barplot(predictor.index, rescaled_values, alpha=0.9)
+    else:
+        ax = sns.barplot(predictor.index, predictor.values, alpha=0.9)
+    
+    if meta_data_img is not None:
+        set_meta_data_img_hist(ax, meta_data_img, labels_list, values_list)
+    
+    if save_fig is True:
+        ax.savefig(os.path.join(dest_fig, fig_name))
+        pass
+    pass
+
+
+def show_pie_by_variable_from_df(df: pd.DataFrame, variable_name: str, save_fig=False, dest_fig: str="stats_report_figures", fig_name="a_pie_chart.png", meta_data_img: dict=None) -> None:
+    assert df is not None, "df is None"
+    assert variable_name in df.columns, f"variable name: {variable_name} is not a column name of input dataframe"
+    
+    check_and_create_dirs(dest_fig)
+    
+    # labels_list = "title,ylabel,xlabel".split(",")
+    # values_list = "Pie,Y,X".split(",")
+
+    predictor = df[f"{variable_name}"].value_counts()
+    tmp_df = pd.DataFrame(data=predictor.values, columns=[f"{variable_name}"], index=predictor.index)
+    tmp_df.plot.pie(y=f"{variable_name}", figsize=(5, 5))
+    
+    if meta_data_img is not None:
+        # set_meta_data_img_hist(ax, meta_data_img, labels_list, values_list)
+        pass
+    
+    if save_fig is True:
+        plt.savefig(os.path.join(dest_fig, fig_name))
+        pass
+    pass
+
+# --------------------------------------------------------------------------- #
+# Utils Functions
+# --------------------------------------------------------------------------- #
+
+def check_and_create_dirs(dir_name: str) -> None:
+    try: os.makedirs(dir_name)
+    except: pass
+    pass
+
+
+def check_perform_rescaling_technique(data, technique_name: str, techniques_dict: dict) -> object:
+    tmp_technique_name: str= technique_name.lower()
+    if tmp_technique_name not in techniques_dict.keys():
+        raise f"Error: {technique_name} is not allowed."
+        
+    a_scaler = techniques_dict[f"{tmp_technique_name}"]
+    if tmp_technique_name != 'normalize':
+        pred_rescaled = a_scaler.fit_transform(data[:,np.newaxis]).ravel()
+    else:
+        pred_rescaled= a_scaler(data[:,np.newaxis], axis=0).ravel()
+    return pred_rescaled
+
+
+def set_meta_data_img_hist(ax, meta_data_img:dict, labels_list: list, values_list: list):
+
+    for ii, label_img in enumerate(labels_list):
+        if label_img not in meta_data_img.keys():
+            meta_data_img[f"{label_img}"] = values_list[ii]
+        pass
+    ax.set_title('{}'.format(meta_data_img["title"]))
+    ax.set_ylabel('{}'.format(meta_data_img["ylabel"]))
+    ax.set_xlabel('{}'.format(meta_data_img["xlabel"]))
+    pass
